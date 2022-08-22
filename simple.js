d315f0c39c215825da9ea96f4b83f2b9bc65d795
JFernandez0524 <njphonefix@gmail.com>
2022-08-22 09:07:44 -0400

    finished video exercises

--- simple.js
+++ simple.js
@@ -1,28 +1,46 @@
-var low     = require('lowdb');
-var fs      = require('lowdb/adapters/FileSync');
+var low = require('lowdb');
+const { write } = require('lowdb/adapters/FileSync');
+var fs = require('lowdb/adapters/FileSync');
 var adapter = new fs('db.json');
-var db      = low(adapter);
+var db = low(adapter);
 
 // init the data store
 // ---------------------------
-// YOUR CODE
+db.defaults({ posts: [] }).write();
 
 // add post
 // ----------------------------
-// YOUR CODE
+
+// db.get('posts')
+//   .push({ id: 5, title: 'Unpublished post 1', published: false })
+//   .write();
+// db.get('posts')
+//   .push({ id: 6, title: 'Unpublished post 2', published: false })
+//   .write();
+
+console.log(db.get('posts').value());
 
 // count posts
 // ----------------------------
-// YOUR CODE
+console.log('Number of posts :' + db.get('posts').size().value());
 
 // find all posts ids
 // ----------------------------
-// YOUR CODE
+console.log('post ids: ' + db.get('posts').map('id').value());
 
 // all matches of published false
 // ----------------------------
 // YOUR CODE
 
+console.log(
+  'all matches of published false: ' +
+    JSON.stringify(db.get('posts').filter({ published: false }).value())
+);
+
 // find post with published false
 // ----------------------------
 // YOUR CODE
+console.log(
+  'find post with published false: ' +
+    JSON.stringify(db.get('posts').find({ published: false }).value())
+);
